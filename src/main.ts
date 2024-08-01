141952158e7e1ff5952c76fcb03a807700cb7236
Murilo <mbalves1@outlook.com>
2024-08-01 18:19:25 -0300

    "Switch from HTTP to RabbitMQ microservice transport"

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('Main');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'], // aqui url nao temos
+      queue: 'admin-backend',
+    },
+  });
+
+  await app.listen(() => logger.log('Microserivces is listening'));
 }
 bootstrap();
